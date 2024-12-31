7d111529aad110aacd6a72206139444e9f5c0eae
Aiman03-del <ausiaam83@gmail.com>
2024-12-31 09:14:54 +0600

    improve responsiveness for MyOrders and OrderCard components

--- src/pages/MyOrders/MyOrders.jsx
+++ src/pages/MyOrders/MyOrders.jsx
@@ -82,9 +82,11 @@ const MyOrders = () => {
   return (
     <div className="container mx-auto px-4 py-8">
       <Toaster position="top-center" reverseOrder={false} />
-      <h1 className="text-2xl font-bold mb-6">My Orders</h1>
+      <h1 className="text-2xl font-bold mb-6 text-center md:text-left">
+        My Orders
+      </h1>
       <motion.div
-        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4"
+        className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6"
         initial="hidden"
         animate="visible"
         variants={{
--- src/pages/MyOrders/OrderCard.jsx
+++ src/pages/MyOrders/OrderCard.jsx
@@ -7,20 +7,20 @@ import { Tooltip } from "react-tooltip";
 const OrderCard = ({ order, handleDelete }) => {
   return (
     <motion.div
-      className="rounded-lg p-4 shadow-md flex flex-col justify-between hover:shadow-lg transition-shadow duration-300"
+      className="rounded-lg p-4 shadow-md flex flex-col justify-between hover:shadow-lg transition-shadow duration-300 "
       initial={{ opacity: 0, y: 50 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, scale: 0.9 }}
       layout
     >
-      <div className="w-full px-6">
+      <div className="w-full">
         <img
           src={order.image}
           alt={order.foodName}
           className="w-full h-48 object-cover rounded-md mb-4"
         />
       </div>
-      <div className="flex items-center justify-between">
+      <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between">
         <div>
           <h2 className="text-lg font-semibold">{order.foodName}</h2>
           <p className="text-sm">Price: ${order.price}</p>
@@ -32,12 +32,12 @@ const OrderCard = ({ order, handleDelete }) => {
         </div>
         <motion.button
           onClick={() => handleDelete(order._id)}
-          className="mt-4 text-red-600  px-4 py-2 rounded-md hover:text-gray-700"
+          className="mt-4 text-red-600 px-4 py-2 rounded-md hover:text-gray-700 sm:mt-0"
           whileHover={{ scaleY: 1.05 }}
           whileTap={{ scaleY: 1 }}
           transition={{ duration: 1 }}
           data-tooltip-id="delete"
-          data-tooltip-content="delete"
+          data-tooltip-content="Delete"
         >
           <RiDeleteBinFill className="text-2xl" />
         </motion.button>
